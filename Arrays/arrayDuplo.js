971fc7a6cc3d538dd38b3b281ba178f575517bdc
Gabriel.Arrien <garrien1@gmail.com>
2022-02-10 08:32:25 -0400

    Array 2nd class

---
+++ Arrays/arrayDuplo.js
@@ -0,0 +1,6 @@
+const alunos = ["João", "Juliana", "Caio", "Ana"];
+const mediaAlunos = [10, 7, 9 ,6]
+
+let listaDeNotasDosAlunos = [alunos, mediaAlunos];
+//primeiro[] qual array, segundo[] posição dentro daquele array
+console.log(`${listaDeNotasDosAlunos[0][0]}, sua média é: ${listaDeNotasDosAlunos[1][0]}`);
