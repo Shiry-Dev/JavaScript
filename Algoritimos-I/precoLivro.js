ac9fc8300d355fbb3f474b3f5ec9e486ce50f347
Gabriel.Arrien <garrien1@gmail.com>
2022-02-14 08:15:01 -0400

    Alg 1st class

---
+++ Algoritimos-I/precoLivro.js
@@ -0,0 +1,11 @@
+const livros = require('./listaLivros');
+
+let maisBarato = 0;
+
+for(let atual = 0; atual < livros.length; atual++){
+    if(livros[atual].preco < livros[maisBarato].preco){
+        maisBarato = atual;
+    }
+}
+
+console.log(`O livro mais barato é: ${livros[maisBarato].preco}\n Seu título é: ${livros[maisBarato].titulo}`);
