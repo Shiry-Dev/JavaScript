07c823b76c9dc5f67e2359bbd0f91f8da9a280e3
Gabriel.Arrien <garrien1@gmail.com>
2022-02-14 13:59:24 -0400

    Alg 4th class

---
+++ Algoritimos/arrays.js
@@ -0,0 +1,39 @@
+const edFolha = [{
+    titulo: "JavaScript",
+    preco: 15
+},{
+    titulo: "PHP",
+    preco: 25
+},{
+    titulo: "JAVA",
+    preco: 30
+},{
+    titulo: "Elixir",
+    preco: 45
+},{
+    titulo: "Go",
+    preco: 50
+}];
+
+const edGalho = [{
+    titulo: "Rust",
+    preco: 20
+},{
+    titulo: "C++",
+    preco: 22
+},{
+    titulo: "C",
+    preco: 28
+},{
+    titulo: "Scala",
+    preco: 33
+},{
+    titulo: "Ruby",
+    preco: 35
+},{
+    titulo: "Python",
+    preco: 40
+}];
+
+
+module.exports = {edFolha, edGalho};
